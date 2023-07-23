585e5e129ba1320a2919291a8aee707204fb5857
kingsley mcdonald <kingsleymcdonald@outlook.com>
2023-07-23 11:56:13 +0100

    feat: show online users in bot status

--- bot/__main__.py
+++ bot/__main__.py
@@ -2,27 +2,57 @@ import asyncio
 import os
 import traceback
 
-from discord import Game, Intents
-from discord.ext.commands import Bot
+from discord import Activity, ActivityType, Intents, Status
+from discord.ext import commands, tasks
 
-from bot.utils import send_error
+from bot.utils import api_get, send_error
 
 intents = Intents.default()
 intents.message_content = True
 
-chatot = Bot(
+chatot = commands.Bot(
     command_prefix="!",
-    activity=Game("with wigglytuff"),
     intents=intents
 )
 
+chatot.current_status = None
 
-@chatot.event
+
+@tasks.loop(seconds=20)
+async def status_loop():
+    api_status, response = await api_get(version=1, endpoint="get_player_count")
+
+    bot_status = Status.idle
+    activity_type = ActivityType.watching
+    message = "osu! alone :("
+
+    if api_status != 200:
+        bot_status = Status.dnd
+        activity_type = ActivityType.watching
+        message = "the server fail"
+
+    elif ppl_online := response["counts"]["online"]:
+        bot_status = Status.online
+        activity_type = ActivityType.playing
+        message = (
+            f"with {ppl_online} player{'s' if ppl_online > 1 else ''} online!"
+        )
+
+    activity = Activity(type=activity_type, name=message)
+
+    if chatot.current_status != message:
+        print("status update!", flush=True)
+        await chatot.change_presence(activity=activity, status=bot_status)
+        chatot.current_status = message
+
+
+@chatot.listen()
 async def on_ready():
+    status_loop.start()
     print("ready!", flush=True)
 
 
-@chatot.event
+@chatot.listen()
 async def on_command_error(ctx, err):
     await send_error(
         ctx,
